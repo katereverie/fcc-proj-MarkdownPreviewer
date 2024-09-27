75e6e463f5a014c696354ce54f23aa4d90c452a9
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-09-27 09:47:02 +0200

    test update

--- markdown-previewer/src/Footer.jsx
+++ markdown-previewer/src/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
 
     return (
         <footer>
-            <p>&copy; {new Date().getFullYear()} <span id="author">Katereverie</span></p>
+            <p>&copy; {new Date().getFullYear()} <span id="author">Katereverietesting</span></p>
         </footer>
     );
 
